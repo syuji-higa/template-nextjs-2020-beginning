9df4c295cc5717a199c51abbeb408711631deb39
syuji-higa <syuji-higa@gmail.com>
2020-03-14 07:36:39 +0900

    update refactor puppeteer-storyshots

--- src/__tests__/puppeteer-storyshots-ipad.runner.ts
+++ src/__tests__/puppeteer-storyshots-ipad.runner.ts
@@ -2,10 +2,8 @@ import initStoryshots from '@storybook/addon-storyshots'
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
 import devices from 'puppeteer/DeviceDescriptors'
 
-const iPhone = devices['iPad']
-
 const customizePage: any = (page) => {
-  return page.emulate(iPhone)
+  return page.emulate(devices['iPad'])
 }
 
 initStoryshots({
--- src/__tests__/puppeteer-storyshots-iphone8.runner.ts
+++ src/__tests__/puppeteer-storyshots-iphone8.runner.ts
@@ -2,10 +2,8 @@ import initStoryshots from '@storybook/addon-storyshots'
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer'
 import devices from 'puppeteer/DeviceDescriptors'
 
-const iPhone = devices['iPhone 8']
-
 const customizePage: any = (page) => {
-  return page.emulate(iPhone)
+  return page.emulate(devices['iPhone 8'])
 }
 
 initStoryshots({
